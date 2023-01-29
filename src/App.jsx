c0d99253365e06f47b4fdc9b0263ad4c19de7070
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2023-01-30 02:37:50 +0530

    News App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import News from './components/News'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <News />
+      </div>
+    )
+  }
+}
+
